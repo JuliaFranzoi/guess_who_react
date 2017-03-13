660b74b7f4f9279e1692ffa14d8d164529dfa5d3
julia franzoi <julia_franzoi@hotmail.com>
2017-03-13 09:27:03 +0000

    commit fix

--- client/src/components/CharacterDetail.jsx
+++ client/src/components/CharacterDetail.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 var CharacterDetail = function(props){
 
- var charDetails = []
- for char props.characters.map(function(char, index){
+ var charDetails = props.characters.map(function(char, index){
   return (<div id="characterDetails">
-    <p key={index}>{char.hair}</p>
+    <p key={index}>{char.name}</p>
     </div>
   );
  })
